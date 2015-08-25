a2c785e4438b1afedbd775cd90e00f7bc4370354
James Lopez <james.lopez@vodafone.com>
2015-08-25 23:11:14 +0100

    updated spec and added basic model and controller

--- spec/controllers/subscribers_controller_spec.rb
+++ spec/controllers/subscribers_controller_spec.rb
@@ -1,29 +1,30 @@
-describe SubscriberController do
-  let(:test_email) {'test@dailyrandom.test'}
-  let(:invalid_email) {'@invalid'}
-  it "should get the subscription index page" do
-    get 'index'
-    expect(response).to be_success
-  end
+require 'rails_helper'
 
-  it "should create a new subscription" do
-    # note the fact that you should remove the "validate_session" parameter if this was a scaffold-generated controller
-    expect{
-      post :create, subscriber: {email: test_email}
-    }.to change(Subscriber,:count).by(1)
-  end
+describe SubscribersController, type: :controller do
+    let(:test_email) {'test@dailyrandom.test'}
+    let(:invalid_email) {'@invalid'}
 
-  it "should not create a new subscription entering an empty email" do
-    # note the fact that you should remove the "validate_session" parameter if this was a scaffold-generated controller
-    expect{
-      post :create, subscriber: {email: ''}
-    }.not_to change(Subscriber, :count).by(1)
-  end
+    it "should get the subscription index page" do
+      get 'index'
+      expect(response).to be_success
+    end
 
-  it "should not create a new subscription entering an invalid email" do
-    # note the fact that you should remove the "validate_session" parameter if this was a scaffold-generated controller
-    expect{
-      post :create, subscriber: {email: test_email}
-    }.not_to change(Subscriber, :count).by(1)
-  end
+    it "should create a new subscription" do
+      expect{
+        post :create, subscriber: {email: test_email}
+      }.to change(Subscriber, :count).by(1)
+    end
+
+    it "should not create a new subscription entering an empty email" do
+      expect{
+        post :create, subscriber: {email: ''}
+      }.not_to change(Subscriber, :count).by(1)
+    end
+
+    it "should not create a new subscription entering an invalid email" do
+      expect{
+        post :create, subscriber: {email: test_email}
+      }.not_to change(Subscriber, :count).by(1)
+    end
+
 end
